5d0395f8dd058eff8d0fbf41b7bcc94063cc782c
Ilja Busch <ilja.ragnar@gmail.com>
2021-09-18 00:58:50 +0200

    Added home and footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <NavBar></NavBar>
-    <router-view/>
+  <div class="relative min-h-screen">
+    <div class="pb-24">
+      <NavBar></NavBar>
+      <router-view/>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
 
 @Component({
   components: {
     NavBar,
+    Footer,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-24 flex flex-row justify-between text-gray-400 bg-gray-800 poppins p-4 bottom-0 absolute w-full">
+    <div>© Alphase 2019 - {{ currentYear }} | All rights reserved</div>
+    <div>We 💜 open source</div>
+    <div>Mady by Ilja Busch aka <a href="//github.com/justmedev" class="text-gray-300 underline" target="_blank">justmedev</a></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {
+  private currentYear = (new Date()).getFullYear();
+}
+</script>
+
+<style scoped>
+.poppins {
+  font-family: Poppins, sans-serif;
+}
+</style>
