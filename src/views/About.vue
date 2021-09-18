b492d476d1d7817e2c0892a3850c6576175bed5a
Ilja Busch <ilja.ragnar@gmail.com>
2021-09-18 15:58:04 +0200

    Finished about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="mx-2">
     <div class="max-w-4xl m-auto my-3">
       <div class="mb-16 mt-6">
-        <span class="text-gray-700 font-bold text-4xl">Our history and our people</span>
+        <span class="text-gray-700 font-bold text-4xl">Our history</span>
         <hr>
         <div class="text-lg">
           On the 17th of August, Elias has posted a recruitment messages in the Brackeys Community
@@ -20,6 +20,57 @@
           decided to rename our team to Alphase and our Discord bot to Alphabot. And now we're here!
         </div>
       </div>
+
+      <div class="mt-6">
+        <span class="text-gray-700 font-bold text-4xl">Our people</span>
+        <hr>
+        <div class="mt-2 ml-2">
+          <div class="flex items-center">
+            <img src="@/assets/elias.jpg" alt="EliasVal Profile Picture" class="h-10 rounded-full">
+            <div class="ml-2">
+              EliasVal:
+              <span class="text-gray-800">
+              {{ ages.elias }}y/o developer currently working on our new game DWAG.
+                (<a href="//github.com/EliasVal" target="_blank">visit on Github</a>)
+             </span>
+            </div>
+          </div>
+
+          <div class="flex items-center mt-2">
+            <img src="@/assets/ilja.png" alt="justmedev Profile Picture" class="h-10 rounded-full">
+            <div class="ml-2">
+              justmedev:
+              <span class="text-gray-800">
+              {{ ages.ilja }}y/o developer currently working on the website and our blog.
+                (<a href="//github.com/justmedev" target="_blank">visit on Github</a>)
+             </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  private birtDates = {
+    ilja: new Date(2007, 1, 27),
+    elias: new Date(2005, 12, 25),
+  };
+  // noinspection JSUnusedLocalSymbols
+  private ages = {
+    ilja: (new Date()).getFullYear() - this.birtDates.ilja.getFullYear(),
+    elias: (new Date()).getFullYear() - this.birtDates.elias.getFullYear(),
+  };
+}
+</script>
+
+<style scoped>
+a {
+  @apply text-blue-500 underline hover:text-green-800 transition-all;
+}
+</style>
