91c353d8f6208ffdb42b2b385fe2ab4fc0661b77
Ilja Busch <ilja.ragnar@gmail.com>
2021-09-18 15:31:11 +0200

    Added about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="mx-2">
+    <div class="max-w-4xl m-auto my-3">
+      <div class="mb-16 mt-6">
+        <span class="text-gray-700 font-bold text-4xl">Our history and our people</span>
+        <hr>
+        <div class="text-lg">
+          On the 17th of August, Elias has posted a recruitment messages in the Brackeys Community
+          Discord Server. And a team of 3 was quickly assembled. We were only 3 programmers at the
+          beginning but one of us has invited a pixel artist, and Elias has invited a Music
+          Designer. We had an idea for a project, a game that is based on the Area 51 raid that was
+          supposed to take place in a month. But the game took sometime to make, and the raid was no
+          longer relevant, so we abandoned the project. At that time, A programmer and the pixel
+          artists have left. But a new artist had joined by that time. So we started working a new
+          horror-ish game, and some progress screenshots exist in the Dev Blog page. But the game
+          didn't go that far. After that we made a mobile game, Color Changer (Currently
+          Unavailable). After publishing v3.0, the team got into an argument and the other
+          programmer left. By that time, a programmer joined the team. We made another mobile game,
+          Jumper. After that we started working on DWAG. Due to the Area 51 hype being over, we
+          decided to rename our team to Alphase and our Discord bot to Alphabot. And now we're here!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
